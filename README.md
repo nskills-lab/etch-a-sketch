c26d09117ea5af55505b623fb30de98e9d0e686a
Nina Rey <reynina.w@gmail.com>
2021-08-15 18:35:03 -0600

    Implement game:
    1. Add grids
    2. Add JS methods to create squares based on the amount, to make grid divs change color when your mouse passes over them, to clear the grid color, to reset square amounts back to baseline
    3. Add styling to buttons and grid squares

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+
+My implementation of Etch a sketch game.  
+Based on the [The Odin Project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project) assignment.
--- script.js
+++ script.js
@@ -1,40 +1,85 @@
 const container = document.querySelector("#container");
 const childId = "box";
-const clearBtn = document.querySelector("#clear");
+const clearBtn = document.querySelector("#eraser");
 const zoomOutBtn = document.querySelector("#z-out");
+const resetBtn = document.querySelector("#reset");
+let gridAmt = 16;
 
 
+function createGridAmount(amount) {
 
-for (let count = 1; count <=256; count++){
-    let temp = document.createElement('div');
-    temp.setAttribute("id", childId+count);
-    container.appendChild(temp);
+    if (amount > 56) {
+        amount = 56;
+        zoomOutBtn.disabled = true;
+    }
+    let limit = Math.pow(amount, 2);
+
+    container.setAttribute("style", `--grid-amount: ${amount}`);
+    for (let count = 1; count <= limit; count++) {
+        let temp = document.createElement('div');
+        temp.setAttribute("id", childId + count);
+        container.appendChild(temp);
+    }
+
 }
 
-function changeColor(e){
+/*
+Function that creates a grid based on the amount given and then 
+sets a listener on the newly created grid to change the color 
+when when your mouse passes over them.
+*/
+function loadGrid(defaultGrid) {
+    createGridAmount(defaultGrid);
+    document.querySelectorAll("#container>div").forEach(box => {
+        box.addEventListener("mouseover", changeColor);
+    });
+}
+
+/*
+Function that removes the old grid and creates the new one.
+*/
+function zoomOut() {
+    removeOldGrid();
+    gridAmt += 10
+    loadGrid(gridAmt)
+}
+
+function removeOldGrid() {
+    clearColor();
+    document.querySelectorAll("#container>div").forEach(n => n.remove());
+}
+
+function changeColor() {
     this.setAttribute("style", `background-color: ${randomRGBA()}`);
 }
 
-function randomRGBA(){
+/*
+Function that generates a random RGBA color.
+*/
+function randomRGBA(e) {
     let red = Math.floor(Math.random() * 256);;
     let green = Math.floor(Math.random() * 256);;
     let blue = Math.floor(Math.random() * 256);;
-    return  `rgba(${red}, ${green}, ${blue})`;
+    return `rgba(${red}, ${green}, ${blue})`;
 
 }
 
-
-function clear(e){
-    boxes.forEach(box => {
+function clearColor(e) {
+    document.querySelectorAll("#container>div").forEach(box => {
         box.setAttribute("style", "background-color: white");
     });
-    
+
 }
-const boxes = document.querySelectorAll("#container>div");
-boxes.forEach(box => {
-    box.addEventListener("mouseover", changeColor);
-});
 
-clearBtn.addEventListener("click", clear);
+function resetGrid(e) {
+    removeOldGrid();
+    gridAmt = 16;
+    loadGrid(gridAmt);
+    zoomOutBtn.disabled = false;
+}
 
 
+loadGrid(gridAmt);
+zoomOutBtn.addEventListener("click", zoomOut);
+clearBtn.addEventListener("click", clearColor);
+resetBtn.addEventListener("click", resetGrid);
